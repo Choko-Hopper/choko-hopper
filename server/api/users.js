7cf64ca2576d30e5bfdb0f1b521bc3a6d376c9ea
lawyatt88 <lemonawyatt@gmail.com>
2018-01-08 16:46:44 -0500

    added api routes for users

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const { User } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
@@ -7,8 +7,35 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    // attributes: ['id', 'email', ]
   })
     .then(users => res.json(users))
     .catch(next)
 })
+
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  User.create(req.body)
+    .then(user => res.status(201).json(user))
+    .catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => user.update(req.body))
+    .then(updatedUser => res.json(updatedUser))
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => user.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
