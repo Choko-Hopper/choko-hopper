ae20619aa1b72e9bc147575612cb3f32905d7920
Katy Eng <katye610@gmail.com>
2018-01-08 17:19:41 -0500

    made order total class method

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Product = require('./product')
 
 const Order = db.define('order', {
   orderId: {
@@ -15,5 +16,23 @@ const Order = db.define('order', {
     defaultValue: true
   }
 })
+
+Order.findTotal = function(orderId, userId) {
+  Order.findAll({
+    where: {
+      userId,
+      orderId
+    },
+    include: [Product]
+  })
+  .then(orders => {
+    let total = 0
+    orders.forEach(function(order) {
+      total += order.product.price * order.quantity
+    })
+    return total
+  })
+  .catch(err => console.error(err))
+}
 
 module.exports = Order
