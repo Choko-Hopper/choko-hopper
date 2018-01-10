272916ff7f9a37d51502fb69682be5788d6103be
Katy Eng <katye610@gmail.com>
2018-01-10 13:35:20 -0500

    created NewProductForm and added route and store stuff

---
+++ client/components/new-product.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {addProductThunk} from '../store'
+
+/**
+ * COMPONENT
+ */
+const NewProductForm = (props) => {
+console.log("Inside NewProductForm component!")
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit} name="newProductForm">
+        <div>
+          <label htmlFor="productName"><small>Product Name</small></label>
+          <input name="name" type="text" />
+        </div>
+        <div>
+          <label htmlFor="imageUrl"><small>imageUrl</small></label>
+          <input name="imageUrl" type="text" />
+        </div>
+        <div>
+          <label htmlFor="price"><small>Price</small></label>
+          <input name="price" type="number" />
+        </div>
+        <div>
+          <label htmlFor="description"><small>Description</small></label>
+          <input name="description" type="number" />
+        </div>
+        <div>
+        <label htmlFor="description"><small>Is this product in stock?</small></label>
+        <select className="form-control" name="isInStock">
+        <option value ="yes" >YES</option>
+        <option value ="no" >NO</option>
+      </select>
+      </div>
+        <div>
+          <button type="submit">Save New Product</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleSubmit (evt) {
+      evt.preventDefault()
+      const newProduct = {
+        name: evt.target.name.value,
+        imageUrl: evt.target.imageUrl.value,
+        price: evt.target.price.value,
+        description: evt.target.description.value,
+        isInStock: evt.target.isInStock.value
+      }
+      //Dispatch thunk that will send axios request
+      dispatch(addProductThunk(newProduct))
+    }
+  }
+}
+
+export default connect(null, mapDispatch)(NewProductForm)
--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, AllProducts} from './components'
+import {Main, Login, Signup, UserHome, AllProducts, NewProductForm} from './components'
 import {me, products} from './store'
 
 /**
@@ -25,6 +25,7 @@ class Routes extends Component {
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/products" component={AllProducts} />
+            <Route path="/new-product" component={NewProductForm} />
             {
               isLoggedIn &&
                 <Switch>
