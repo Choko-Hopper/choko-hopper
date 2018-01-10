ab56384ae3d31bc3355aa2c9dce83c4040701621
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-10 14:30:47 -0500

    have error thrown for mismatching passwords during signup, still need error to display for users

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,25 +1,38 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import PropTypes from 'prop-types'
-import {auth} from '../store'
+import React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { auth } from "../store"
 
 /**
  * COMPONENT
  */
-const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+const AuthForm = props => {
+  const { name, displayName, handleSubmit, error } = props
+  const isSignUp = props.name === "signup"
 
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
         <div>
-          <label htmlFor="email"><small>Email</small></label>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
           <input name="email" type="text" />
         </div>
         <div>
-          <label htmlFor="password"><small>Password</small></label>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
           <input name="password" type="password" />
         </div>
+        {isSignUp && (
+          <div>
+            <label htmlFor="password">
+              <small>Re-Enter Password</small>
+            </label>
+            <input name="password2" type="password" />
+          </div>
+        )}
         <div>
           <button type="submit">{displayName}</button>
         </div>
@@ -38,30 +51,37 @@ const AuthForm = (props) => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = (state) => {
+const mapLogin = state => {
   return {
-    name: 'login',
-    displayName: 'Login',
+    name: "login",
+    displayName: "Login",
     error: state.user.error
   }
 }
 
-const mapSignup = (state) => {
+const mapSignup = state => {
   return {
-    name: 'signup',
-    displayName: 'Sign Up',
+    name: "signup",
+    displayName: "Sign Up",
     error: state.user.error
   }
 }
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = (dispatch, ownProps) => {
   return {
-    handleSubmit (evt) {
+    handleSubmit(evt, isSignUp) {
       evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(auth(email, password, formName))
+      const password2 = evt.target.password2.value
+      if (formName === "signup" && password !== password2) {
+        const err = new Error("Passwords Do Not Match")
+        err.status = "no match"
+        throw err
+      } else {
+        dispatch(auth(email, password, formName))
+      }
     }
   }
 }
