79a6fe64962f265f7da938d4b97c3a12b3021d37
lawyatt88 <lemonawyatt@gmail.com>
2018-01-10 12:09:24 -0500

    added admin security for user routes and orders

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -16,6 +16,7 @@ describe('User routes', () => {
 
     beforeEach(() => {
       return User.create({
+        id: 1,
         email: codysEmail
       })
     })
@@ -26,6 +27,16 @@ describe('User routes', () => {
         .expect(200)
         .then(res => {
           expect(res.body).to.be.an('array')
+          expect(res.body[0].email).to.be.equal(codysEmail)
+        })
+    })
+
+    it('GET /api/users/:id', () => {
+      return request(app)
+        .get('/api/users/1')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('object')
           expect(res.body[0].email).to.be.equal(codysEmail)
         })
     })
