d681d8d0b280c4cf00985fb7bfeeec8641a3d6cc
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-10 15:17:42 -0500

    password re-enter field added on signup

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import { auth } from "../store"
@@ -6,42 +6,52 @@ import { auth } from "../store"
 /**
  * COMPONENT
  */
-const AuthForm = props => {
-  const { name, displayName, handleSubmit, error } = props
-  const isSignUp = props.name === "signup"
+class AuthForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      passwordError: ""
+    }
+  }
+  render() {
+    const { name, displayName, error } = this.props
+    const handleSubmit = this.props.handleSubmit.bind(this)
+    const isSignUp = this.props.name === "signup"
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        {isSignUp && (
+    return (
+      <div>
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+          </div>
           <div>
             <label htmlFor="password">
-              <small>Re-Enter Password</small>
+              <small>Password</small>
             </label>
-            <input name="password2" type="password" />
+            <input name="password" type="password" />
           </div>
-        )}
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
-      <a href="/auth/facebook">{displayName} with Facebook</a>
-    </div>
-  )
+          {isSignUp && (
+            <div>
+              <label htmlFor="password">
+                <small>Re-Enter Password</small>
+              </label>
+              <input name="password2" type="password" />
+            </div>
+          )}
+          <div>
+            <button type="submit">{displayName}</button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+          <div>{this.state.passwordError} </div>
+        </form>
+        <a href="/auth/google">{displayName} with Google</a>
+        <a href="/auth/facebook">{displayName} with Facebook</a>
+      </div>
+    )
+  }
 }
 
 /**
@@ -76,9 +86,7 @@ const mapDispatch = (dispatch, ownProps) => {
       const password = evt.target.password.value
       const password2 = evt.target.password2.value
       if (formName === "signup" && password !== password2) {
-        const err = new Error("Passwords Do Not Match")
-        err.status = "no match"
-        throw err
+        this.setState({ passwordError: "Passwords Do Not Match" })
       } else {
         dispatch(auth(email, password, formName))
       }
