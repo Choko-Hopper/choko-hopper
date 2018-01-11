8855684727ae862700f102ad16048843911520ce
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-01-11 14:39:39 -0500

    Update history.js
    cleaned up console.log

--- client/history.js
+++ client/history.js
@@ -1,6 +1,5 @@
 import createHistory from 'history/createBrowserHistory'
 import createMemoryHistory from 'history/createMemoryHistory'
-console.log('I AM HERE!', process.env.NODE_ENV)
 //const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory()
 
 let history
