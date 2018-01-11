ecfbfd963485e2144fcf4de65822c0de7a459fc4
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-01-11 14:41:59 -0500

    Update index.js
    merge conflict, removed isAdmin function as it is required in from a separate module now

--- server/api/index.js
+++ server/api/index.js
@@ -2,19 +2,6 @@ const router = require('express').Router()
 const isAdmin = require('./isAdmin')
 module.exports = router
 
-<<<<<<< HEAD
-const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin || process.env.NODE_ENV === 'test') {
-    next()
-  } else {
-    let error = new Error('Unauthorized Action')
-    error.status = 403
-    next(error)
-  }
-}
-
-=======
->>>>>>> 8074983be4f4b90a601d3de52ae26ea485efd792
 router.use('/users', isAdmin, require('./users'))
 router.use('/products', require('./products'))
 router.use('/orders', isAdmin, require('./orders'))
