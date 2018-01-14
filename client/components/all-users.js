a2cdee5d5c402d869bb35dfd3ad458c44e9b8da0
Annabel Lau <annabel.n.lau@gmail.com>
2018-01-13 21:23:04 -0500

    added delete feature to all users page

--- client/components/all-users.js
+++ client/components/all-users.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
+import {deleteUserThunk} from '../store'
 
 function AllUsers(props) {
 
     return (
       <div>
       {props.currentUser && !props.currentUser.isAdmin ?
-        <h3>Sorry, you don't have access to this page.</h3> :
-
-        
+        <h3>Sorry, you don't have access to this page.</h3> :   
         <div>
         
             <h1>All Users</h1>
@@ -18,7 +17,10 @@ function AllUsers(props) {
                     props.allUsers.map(user => {
                         return (
                             <li key={user.id}>
-                                <span>{user.email}</span>
+                                <span>{user.email}     </span>
+                               
+                                <button id={user.id} onClick={props.handleClick}>Delete</button>
+                               
                             </li>
                         )
                     })
@@ -37,5 +39,14 @@ const mapState = function(state) {
     }
 }
 
+const mapDispatch = function(dispatch) {
+    return {
+        handleClick(evt){
+            evt.preventDefault()
+            const userId = evt.target.id
+            dispatch(deleteUserThunk(userId))
+        }
+    }
+}
 
-export default connect(mapState, null)(AllUsers)
+export default connect(mapState, mapDispatch)(AllUsers)
--- client/store/allUsers.js
+++ client/store/allUsers.js
@@ -4,8 +4,10 @@ import history from '../history'
 const allUsers = []
 
 const GET_USERS = 'GET_USERS'
+const DELETE_USER = 'DELETE_USER'
 
-const getUsers = users => ({ type: GET_USERS, users })
+export const getUsers = users => ({ type: GET_USERS, users })
+export const deleteUser = userId => ({ type: DELETE_USER, userId })
 
 export const users = () =>
   dispatch =>
@@ -14,13 +16,23 @@ export const users = () =>
         dispatch(getUsers(res.data)))
       .catch(err => console.log(err))
 
+export const deleteUserThunk = (userId) =>
+  dispatch =>
+     axios.delete(`/api/users/${userId}`)
+       .then(res =>
+          dispatch(deleteUser(userId)))
+      .catch(err => console.log(err))
+
       export default function (state = allUsers, action) {
         switch (action.type) {
           case GET_USERS:
             return [...allUsers, ...action.users]
+          case DELETE_USER:
+            return state.filter(user => {
+              return user.id !== +action.userId
+            })
 
           default:
             return state
         }
       }
-      
