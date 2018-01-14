4e39a574e96698d2f2e0faa10278c82d4f8e2732
Katy Eng <katye610@gmail.com>
2018-01-13 23:14:23 -0500

    put userId on each new order

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,17 +1,27 @@
 const router = require('express').Router()
 const { Order, LineItems } = require('../db/models')
+const isAdmin = require('./isAdmin')
 module.exports = router
 
-router.get('/', (req, res, next) => {
+router.get('/', isAdmin, (req, res, next) => {
     Order.findAll()
         .then(orders => res.json(orders))
         .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-    Order.findById(req.params.id)
-        .then(order => res.json(order))
-        .catch(next)
+router.get('/:orderId', (req, res, next) => {
+  const { orderId } = req.params
+  Order.findById(orderId)
+    .then(order => {
+      if (req.user && req.user.id === order.userId) {
+        res.json(order)
+      } else {
+        const err = new Error('This order belongs to another')
+        err.status = 401
+        next(err)
+      }
+    })
+    .catch(next)
 })
 
 router.post('/', (req, res, next) => {
@@ -19,7 +29,8 @@ router.post('/', (req, res, next) => {
   //{userEmail, shippingAddress, cart}
   let orderInfo = {
     userEmail: req.body.userEmail,
-    shippingAddress: req.body.shippingAddress
+    shippingAddress: req.body.shippingAddress,
+    userId: req.user.id
   }
   let cart = req.body.cart
 
@@ -34,14 +45,14 @@ router.post('/', (req, res, next) => {
         .catch(next)
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', isAdmin, (req, res, next) => {
     Order.findById(req.params.id)
         .then(order => order.update(req.body))
         .then(updatedOrder => res.json(updatedOrder))
         .catch(next)
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', isAdmin, (req, res, next) => {
     Order.findById(req.params.id)
         .then(order => order.destroy())
         .then(() => res.send(204))
