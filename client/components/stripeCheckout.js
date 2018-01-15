8658c4301864fca620bef84f19011480537f2fa6
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-15 16:38:12 -0500

    added description to stripe

--- client/components/stripeCheckout.js
+++ client/components/stripeCheckout.js
@@ -5,9 +5,10 @@ import StripeCheckout from 'react-stripe-checkout'
 import { submitCart } from '../store'
 
 const STRIPE_PUBLISHABLE = 'pk_test_BjdBrWKAZMF0Lwo9Pncz4SxF'
-const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://choko-hopper.herokuapp.com/auth/checkout'
-  : 'http://localhost:8080/auth/checkout'
+const PAYMENT_SERVER_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://choko-hopper.herokuapp.com/auth/checkout'
+    : 'http://localhost:8080/auth/checkout'
 
 const CURRENCY = 'USD'
 
@@ -27,20 +28,39 @@ const onToken = (amount, description, handleSuccess, cart) => token =>
     })
     .catch(() => alert('Payment Error'))
 
-const Checkout = ({ description, amount, handleSuccess, cart}) => (
-  <StripeCheckout
-    name='mmmm'
-    description='still chocolate'
-    amount={fromUSDToCent(100)}
-    token={onToken(amount, description, handleSuccess, cart)}
-    currency={CURRENCY}
-    stripeKey={STRIPE_PUBLISHABLE}
-  />
+const Checkout = ({ description, amount, handleSuccess, cart }) => (
+  <div>
+    <StripeCheckout
+      name="Your Chocolate Order"
+      description="still chocolate"
+      amount={fromUSDToCent(amount)}
+      token={onToken(amount, description, handleSuccess, cart)}
+      currency={CURRENCY}
+      stripeKey={STRIPE_PUBLISHABLE}
+    />
+  </div>
 )
 
-const mapState = ({ products, cart }) => ({ products, cart })
+const findDescription = (cart, products) => {
+  let cartProducts = cart.cart.map(cartItem => {
+    let result = products.find(product => product.id === cartItem.productId)
+    return result ? result.name : result
+  }
+  )
+  return cartProducts.length
+    ? cartProducts.reduce((accumulator, item) => {
+      return accumulator + ', ' + item
+    })
+    : ''
+}
+
+const mapState = ({ products, cart }) => ({
+  products,
+  cart,
+  description: findDescription(cart, products)
+})
 const mapDispatch = dispatch => ({
-  handleSuccess: (cart) => {
+  handleSuccess: cart => {
     dispatch(submitCart(cart))
   }
 })
