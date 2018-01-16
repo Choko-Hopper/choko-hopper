1c6cc1b033002b6a5418ba0766112724abac8a32
jasminabasurita <jasminejacquelin@gmail.com>
2018-01-16 18:47:17 -0500

    less buggy but not void of bugs

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -10,53 +10,122 @@ import AllProducts from './all-products'
 /**
  * COMPONENT
  */
-const Dashboard = (props) => {
-    console.log('PATH', props.path)
-    let orderDetails
-    let sidebarLinksArr
-    let panelArr
-    let subStrArr=[]
+const Dashboard = props => {
+  console.log('PATH', props.path)
+  let orderDetails
+  let sidebarLinksArr
+  let panelArr
+  let subStrArr = []
 
-    if (props.user.isAdmin) {
-        orderDetails = props.match.params.orderId ? <OrderLineItems userId={props.user.id} match={props.match} history={props.history} /> : <OrderHistory userId={props.user.id} history={props.history} />
-        sidebarLinksArr = ['Order History', 'Authorized Users', 'View All Orders', 'Edit Products']
-        panelArr = [orderDetails, <AllUsers key="allusers" />, <AllOrders key="allorders" />, <AllProducts key="allproducts"  path={props.path}/>]
-    } else  {
-        orderDetails = props.match.params.orderId ? <OrderLineItems userId={props.user.id} match={props.match} history={props.history} /> : <OrderHistory userId={props.user.id} history={props.history} />
-        sidebarLinksArr = ['Order History']
-        panelArr = [orderDetails]
-    }
-    return (
-            <div className="container">
-                <div className="admin-panel row d-flex align-items-stretch">
-                    <div className="sidebar col-3">
-                        <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                        <a className="nav-link active" id="v-pills-userinfo-tab" data-toggle="pill" href="#v-pills-userinfo" role="tab" aria-controls="v-pills-userinfo" aria-selected="false">User Info</a>
-                            {sidebarLinksArr.map( link => {
-                                let substr = link.replace(/\s+/g, '-').toLowerCase()
-                                subStrArr.push(substr)
-                                return <a key={substr} className="nav-link" id={`v-pills-${substr}-tab`} data-toggle="pill" href={`#v-pills-${substr}`} role="tab" aria-controls={`v-pills-${substr}`} aria-selected="true">{link}</a>
-                            })}
-                        </div>
-                    </div>
-                    <div className="col-9">
-                        <div className="tab-content" id="v-pills-tabContent">
-                            <div className="tab-pane fade show active" id="v-pills-userinfo" role="tabpanel" aria-labelledby="v-pills-userinfo-tab" />
-                            {panelArr.map( (panel, i) => {
-                                return <div key={subStrArr[i]} className="tab-pane fade" id={`v-pills-${subStrArr[i]}`} role="tabpanel" aria-labelledby={`v-pills-${subStrArr[i]}-tab`}>{panel}</div>
-                            })}
-                        </div>
-                    </div>
+  if (props.user.isAdmin) {
+    orderDetails = props.match.params.orderId ? (
+      <OrderLineItems
+        userId={props.user.id}
+        match={props.match}
+        history={props.history}
+      />
+    ) : (
+      <OrderHistory userId={props.user.id} history={props.history} />
+    )
+    sidebarLinksArr = [
+      'Order History',
+      'Authorized Users',
+      'View All Orders',
+      'Edit Products'
+    ]
+    panelArr = [
+      orderDetails,
+      <AllUsers key="allusers" />,
+      <AllOrders key="allorders" />,
+      <AllProducts key="allproducts" path={props.path} />
+    ]
+  } else {
+    orderDetails = props.match.params.orderId ? (
+      <OrderLineItems
+        userId={props.user.id}
+        match={props.match}
+        history={props.history}
+      />
+    ) : (
+      <OrderHistory userId={props.user.id} history={props.history} />
+    )
+    sidebarLinksArr = ['Order History']
+    panelArr = [orderDetails]
+  }
+  return (
+    <div className="container">
+      <div className="admin-panel row d-flex align-items-stretch">
+        <div className="sidebar col-3">
+          <div
+            className="nav flex-column nav-pills"
+            id="v-pills-tab"
+            role="tablist"
+            aria-orientation="vertical"
+          >
+            <a
+              className="nav-link active"
+              id="v-pills-userinfo-tab"
+              data-toggle="pill"
+              href="#v-pills-userinfo"
+              role="tab"
+              aria-controls="v-pills-userinfo"
+              aria-selected="false"
+            >
+              User Info
+            </a>
+            {sidebarLinksArr.map(link => {
+              let substr = link.replace(/\s+/g, '-').toLowerCase()
+              subStrArr.push(substr)
+              return (
+                <a
+                  key={substr}
+                  className="nav-link"
+                  id={`v-pills-${substr}-tab`}
+                  data-toggle="pill"
+                  href={`#v-pills-${substr}`}
+                  role="tab"
+                  aria-controls={`v-pills-${substr}`}
+                  aria-selected="true"
+                >
+                  {link}
+                </a>
+              )
+            })}
+          </div>
+        </div>
+        <div className="col-9">
+          <div className="tab-content" id="v-pills-tabContent">
+            <div
+              className="tab-pane fade show active"
+              id="v-pills-userinfo"
+              role="tabpanel"
+              aria-labelledby="v-pills-userinfo-tab"
+            />
+            {panelArr.map((panel, i) => {
+              return (
+                <div
+                  key={subStrArr[i]}
+                  className="tab-pane fade"
+                  id={`v-pills-${subStrArr[i]}`}
+                  role="tabpanel"
+                  aria-labelledby={`v-pills-${subStrArr[i]}-tab`}
+                >
+                  {panel}
                 </div>
-            </div>
-        )
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-function mapStateToProps (state, ownProps){
-    return {
-        user: state.user,
-        path: ownProps.match.path
-    }
+function mapStateToProps(state, ownProps) {
+  return {
+    user: state.user,
+    path: ownProps.match.path
+  }
 }
 
 export default connect(mapStateToProps)(Dashboard)
--- client/components/new-product.js
+++ client/components/new-product.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addOrEditProductThunk } from '../store'
@@ -7,7 +6,7 @@ import { addOrEditProductThunk } from '../store'
  * COMPONENT
  */
 class ProductForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     console.log('loooooooooook', props)
     this.state = {
@@ -19,72 +18,110 @@ class ProductForm extends Component {
     }
 
     this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange(evt){
+  handleChange(evt) {
     this.setState({ [evt.target.name]: evt.target.value })
   }
 
-  handleSubmit(evt) {
-    evt.preventDefault()
-    const productId = this.props.product.id ? +this.props.product.id : null
-    this.props.dispatch(addOrEditProductThunk(this.state, productId))
-    this.setState({
-      name: '',
-      imageUrl: '',
-      price: '',
-      description: '',
-      quantity: '0'
-    })
-  }
-
-  componentDidMount(){
+  componentDidMount() {
     this.setLocalState()
   }
 
-  setLocalState(){
-    this.setState((prevState, props) => this.props.product);
+  setLocalState() {
+    this.setState((prevState, props) => this.props.product)
   }
 
   render() {
     const product = this.props.product
     return (
       <div>
-        {this.props.currentUser && !this.props.currentUser.isAdmin ?
-          <h3>Sorry, you don't have access to this page.</h3> :
+        {this.props.currentUser && !this.props.currentUser.isAdmin ? (
+          <h3>Sorry, you don't have access to this page.</h3>
+        ) : (
           <div>
-            <h3>{this.props.formTitle} {product.name || ''}</h3>
-            <form onSubmit={this.props.handleSubmit} name="newProductForm">
+            <h3>
+              {this.props.formTitle} {product.name || ''}
+            </h3>
+            <form
+              onSubmit={evt => {
+                const productId = this.props.product.id
+                  ? +this.props.product.id
+                  : null
+                this.props.handleSubmit(evt, this.state, productId)
+                this.setState({
+                  name: '',
+                  imageUrl: '',
+                  price: '',
+                  description: '',
+                  quantity: '0'
+                })
+              }}
+              name="newProductForm"
+            >
               <div>
-                <label htmlFor="name"><small>Product Name</small></label>
-                <input onChange={this.handleChange} value={this.state.name} name="name" type="text" />
+                <label htmlFor="name">
+                  <small>Product Name</small>
+                </label>
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.name}
+                  name="name"
+                  type="text"
+                />
               </div>
               <div>
-                <label htmlFor="imageUrl"><small>imageUrl</small></label>
-                <input onChange={this.handleChange} value={this.state.imageUrl} name="imageUrl" type="text" />
+                <label htmlFor="imageUrl">
+                  <small>imageUrl</small>
+                </label>
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.imageUrl}
+                  name="imageUrl"
+                  type="text"
+                />
               </div>
               <div>
-                <label htmlFor="price"><small>Price</small></label>
-                <input onChange={this.handleChange} value={this.state.price} name="price" type="text" />
+                <label htmlFor="price">
+                  <small>Price</small>
+                </label>
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.price}
+                  name="price"
+                  type="text"
+                />
               </div>
               <div>
-                <label htmlFor="description"><small>Description</small></label>
-                <input onChange={this.handleChange} value={this.state.description} name="description" type="text" />
+                <label htmlFor="description">
+                  <small>Description</small>
+                </label>
+                <input
+                  onChange={this.handleChange}
+                  value={this.state.description}
+                  name="description"
+                  type="text"
+                />
               </div>
-  
+
               <div>
-                <select name= "category">
-                <option value="0">Choose a category</option>
-                {this.props.categories.map(category => {
-                  return <option key={category.id} value={category.id}>{category.name}</option>
-                })}
+                <select name="category">
+                  <option value="0">Choose a category</option>
+                  {this.props.categories.map(category => {
+                    return (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    )
+                  })}
                 </select>
               </div>
-  
+
               <div>
                 <div>
-                  <label htmlFor="quantity"><small>Quantity</small></label>
+                  <label htmlFor="quantity">
+                    <small>Quantity</small>
+                  </label>
                   <input name="quantity" type="text" />
                 </div>
               </div>
@@ -93,15 +130,13 @@ class ProductForm extends Component {
               </div>
             </form>
           </div>
-        }
+        )}
       </div>
     )
-
   }
-
 }
 
-const mapNewProduct = (state) => {
+const mapNewProduct = state => {
   return {
     products: [],
     name: 'newProduct',
@@ -111,7 +146,7 @@ const mapNewProduct = (state) => {
   }
 }
 
-const mapEditProduct = (state) => {
+const mapEditProduct = state => {
   return {
     products: state.products,
     name: 'editProduct',
@@ -121,11 +156,12 @@ const mapEditProduct = (state) => {
   }
 }
 
-const mapDispatch = (dispatch, ownProps) => {
-  return {
-    
+const mapDispatch = (dispatch, ownProps) => ({
+  handleSubmit(evt, state, productId) {
+    evt.preventDefault()
+    dispatch(addOrEditProductThunk(state, productId))
   }
-}
+})
 
 export const NewProductForm = connect(mapNewProduct, mapDispatch)(ProductForm)
 export const EditProductForm = connect(mapEditProduct, mapDispatch)(ProductForm)
